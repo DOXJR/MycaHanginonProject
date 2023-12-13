b4d3be04220443524078f3a663d4a0df8f3f711b
MrDTheGreat <darylekazuto@gmail.com>
2023-12-13 10:01:50 +0800

    FirstCommit

---
+++ screens/AboutScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+export default function AboutScreen(){
+  return(
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to Home Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    alignItems: "center",
+    justifyContent:"center",
+  },
+  text:{
+    fontSize:20,
+    fontWeight:"bold",
+    marginBottom:16,
+    textAlign: "center",
+    justifyContent:"center"
+  },
+});
+
---
+++ screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
+export default function HomeScreen({navigation}){
+  return(
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to HomeScreen</Text>
+      <Button title="Go to About" onPress={()=> navigation.navigate("About")}/>     
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems: "center",
+    justifyContent:"center",
+  },
+  text:{
+    fontSize: 24,
+    fontWeight:"bold",
+    marginBottom: 16
+  },
+})
